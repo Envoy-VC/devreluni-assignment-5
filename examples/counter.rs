549a96c650a0978adb77235368aa9619869e11c3
Vedant Chainani <vedantchainani1084@gmail.com>
2024-12-20 03:37:03 +0530

    chore: add alloy scripts

--- examples/counter.rs
+++ examples/counter.rs
@@ -1,78 +1,80 @@
-//! Example on how to interact with a deployed `stylus-hello-world` contract using defaults.
-//! This example uses ethers-rs to instantiate the contract using a Solidity ABI.
-//! Then, it attempts to check the current counter value, increment it via a tx,
-//! and check the value again. The deployed contract is fully written in Rust and compiled to WASM
-//! but with Stylus, it is accessible just as a normal Solidity smart contract is via an ABI.
-
-use ethers::{
-    middleware::SignerMiddleware,
-    prelude::abigen,
-    providers::{Http, Middleware, Provider},
-    signers::{LocalWallet, Signer},
-    types::Address,
-};
-use dotenv::dotenv;
-use eyre::eyre;
-use std::io::{BufRead, BufReader};
 use std::str::FromStr;
-use std::sync::Arc;
 
-/// Your private key file path.
-const PRIV_KEY_PATH: &str = "PRIV_KEY_PATH";
+use dotenv::dotenv;
 
-/// Stylus RPC endpoint url.
-const RPC_URL: &str = "RPC_URL";
+use alloy::{
+    network::EthereumWallet, primitives::U256, providers::ProviderBuilder,
+    signers::local::PrivateKeySigner, sol,
+};
+use eyre::eyre;
 
-/// Deployed pragram address.
-const STYLUS_CONTRACT_ADDRESS: &str = "STYLUS_CONTRACT_ADDRESS";
+sol!(
+    #[allow(missing_docs)]
+    #[sol(rpc)]
+    Counter,
+    "abi/Counter.json"
+);
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     dotenv().ok();
-    let priv_key_path =
-        std::env::var(PRIV_KEY_PATH).map_err(|_| eyre!("No {} env var set", PRIV_KEY_PATH))?;
-    let rpc_url = std::env::var(RPC_URL).map_err(|_| eyre!("No {} env var set", RPC_URL))?;
-    let contract_address = std::env::var(STYLUS_CONTRACT_ADDRESS)
-        .map_err(|_| eyre!("No {} env var set", STYLUS_CONTRACT_ADDRESS))?;
-    abigen!(
-        Counter,
-        r#"[
-            function number() external view returns (uint256)
-            function setNumber(uint256 number) external
-            function increment() external
-        ]"#
-    );
+    let private_key =
+        std::env::var("PRIVATE_KEY").map_err(|_| eyre!("No {} env var set", "PRIVATE_KEY"))?;
+    let rpc_url = std::env::var("RPC_URL").map_err(|_| eyre!("No {} env var set", "RPC_URL"))?;
+    let contract_address = "0xd92773693917f0ff664f85c3cb698c33420947ff";
 
-    let provider = Provider::<Http>::try_from(rpc_url)?;
-    let address: Address = contract_address.parse()?;
+    let signer: PrivateKeySigner = PrivateKeySigner::from_str(&private_key).unwrap();
+    let wallet = EthereumWallet::from(signer.clone());
 
-    let privkey = read_secret_from_file(&priv_key_path)?;
-    let wallet = LocalWallet::from_str(&privkey)?;
-    let chain_id = provider.get_chainid().await?.as_u64();
-    let client = Arc::new(SignerMiddleware::new(
-        provider,
-        wallet.clone().with_chain_id(chain_id),
-    ));
+    let provider = ProviderBuilder::new()
+        .with_recommended_fillers()
+        .wallet(wallet)
+        .on_http(rpc_url.parse()?);
 
-    let counter = Counter::new(address, client);
-    let num = counter.number().call().await;
-    println!("Counter number value = {:?}", num);
+    let counter = Counter::new(contract_address.parse()?, provider.clone());
 
-    let pending = counter.increment();
-    if let Some(receipt) = pending.send().await?.await? {
-        println!("Receipt = {:?}", receipt);
-    }
-    println!("Successfully incremented counter via a tx");
+    let mut count: U256 = counter.number().call().await?._0;
 
-    let num = counter.number().call().await;
-    println!("New counter number value = {:?}", num);
+    println!("Initial Count = {:?}", count);
+
+    // Set Number
+    counter
+        .setNumber(U256::from(10))
+        .send()
+        .await?
+        .watch()
+        .await?;
+
+    count = counter.number().call().await?._0;
+    println!("After Set Number Count = {:?}", count);
+
+    // Multiply Number
+    counter
+        .mulNumber(U256::from(10))
+        .send()
+        .await?
+        .watch()
+        .await?;
+
+    count = counter.number().call().await?._0;
+    println!("After Multiply Number Count = {:?}", count);
+
+    // Add Number
+    counter
+        .addNumber(U256::from(10))
+        .send()
+        .await?
+        .watch()
+        .await?;
+
+    count = counter.number().call().await?._0;
+    println!("After Add Number Count = {:?}", count);
+
+    // Increment Number
+
+    counter.increment().send().await?.watch().await?;
+    count = counter.number().call().await?._0;
+    println!("After Increment Count = {:?}", count);
+
     Ok(())
-}
-
-fn read_secret_from_file(fpath: &str) -> eyre::Result<String> {
-    let f = std::fs::File::open(fpath)?;
-    let mut buf_reader = BufReader::new(f);
-    let mut secret = String::new();
-    buf_reader.read_line(&mut secret)?;
-    Ok(secret.trim().to_string())
 }
